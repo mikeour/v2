6076a0e1e72ab2c5b45ca4184a1bd0c8bf971c77
Michael Roeslein <michaeljroeslein@gmail.com>
2025-12-14 17:30:20 -0800

    feat: add sitemap and fix date element in MDX

---
+++ apps/www/src/app/sitemap.ts
@@ -0,0 +1,22 @@
+import type { MetadataRoute } from "next";
+
+const baseUrl = "https://mikeour.io";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/crafts`, lastModified: new Date() },
+    { url: `${baseUrl}/crafts/scroll-shadows`, lastModified: new Date() },
+    { url: `${baseUrl}/photos`, lastModified: new Date() },
+    { url: `${baseUrl}/music`, lastModified: new Date() },
+    { url: `${baseUrl}/movies`, lastModified: new Date() },
+    {
+      url: `${baseUrl}/blog/elevate-your-web-development-stack-with-tailwind-css`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/blog/leveraging-the-power-of-react-and-typescript`,
+      lastModified: new Date(),
+    },
+  ];
+}
